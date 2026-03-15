4f2d57afc0a18d6feace94518dd6e936836e8bf0
Atharva Joshi <jatharva1701@gmail.com>
2026-03-15 19:02:56 +0530

    add book submission and exchange form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Landing from "./pages/Landing";
 import Profile from "./pages/Profile";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
+import BookDetail from "./pages/BookDetail";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/profile" element={<Profile />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/buybook" element={<BookDetail />} />
       </Routes>
     </BrowserRouter>
   );
--- client/src/pages/Navbar.jsx
+++ client/src/pages/Navbar.jsx
@@ -8,7 +8,7 @@ export default function Navbar({ isprofile }) {
             PowerXchange
         </h1>
       <div className="flex gap-4">
-        <button onClick={() => navigate("/login")} className="text-gray-600 hover:text-indigo-600 font-medium">Login</button>
+        <button onClick={() => navigate("/Login")} className="text-gray-600 hover:text-indigo-600 font-medium">Login</button>
         <button onClick={() => navigate("/signup")} className="bg-indigo-600 text-white px-4 py-2 rounded-lg hover:bg-indigo-700">Sign Up</button>
         
         {!isprofile && <button onClick={() => navigate("/profile")} className="text-gray-600 hover:text-indigo-600 font-medium">My Profile</button>}

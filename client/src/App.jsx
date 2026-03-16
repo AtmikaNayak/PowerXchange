f15e8ab0e992796824eb861dca2a7c88f52b5a69
Atharva Joshi <jatharva1701@gmail.com>
2026-03-16 20:38:12 +0530

    changed bookdetails to buybook jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import Landing from "./pages/Landing";
 import Profile from "./pages/Profile";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
-import BookDetail from "./pages/BookDetail";
+import  Buybook from "./pages/Buybook";
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
         <Route path="/profile" element={<Profile />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/buybook" element={<BookDetail />} />
+        <Route path="/buybook" element={<Buybook />} />
       </Routes>
     </BrowserRouter>
   );
